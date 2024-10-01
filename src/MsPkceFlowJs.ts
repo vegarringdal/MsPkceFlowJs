00f71367a8144f173a0333109d57ccb41847458b
Vegar Ringdal <vegar.ringdal@gmail.com>
2024-10-01 23:58:44 +0200

    refactor

--- src/MsPkceFlowJs.ts
+++ src/MsPkceFlowJs.ts
@@ -12,48 +12,88 @@ export class MsPkceFlowJs {
   #useOldStateLocalStorageKey = "__useOldState";
   #accountLocalStorageKey = "__account";
   #currentAccount: TokenResult | null = null;
-  public useLog = false;
-  #callback: (result: TokenResult) => void;
+  #isActivated = false;
 
-  constructor(authConfig: AuthConfig, callback?: (result: TokenResult) => void) {
+
+  constructor(
+    authConfig: AuthConfig
+  ) {
     this.#authConfig = authConfig;
     this.#currentAccount = null;
-    this.#callback = callback || function(){};
+  
   }
 
-  public addEventLister(listener: (event: any)=>void | {handleEvent:(event: any)=> void}){
-   // todo 
-  }
-
+ 
+  /**
+   * tries to get access toekn
+   * @returns 
+   */
   public getAccessToken() {
-    return this.#currentAccount?.access_token;
+
+    // todo, maybe this should try and refresh token if needed?
+
+    if(!this.#isActivated){
+      return errorResult("You need to call init before using any other functions", null);
+    }
+    if(!this.#currentAccount?.access_token){
+      return errorResult("Missing access token", null);
+    }
+    return successResult(this.#currentAccount?.access_token);
   }
 
+   /**
+   * tries to update token using refresh
+   * @returns 
+   */
   public async updateToken() {
     const token = this.#currentAccount?.refresh_token;
 
-    if (token) {
+    if (!token) {
+      return errorResult("no refresh token available", null);
+    }
+
+    try {
       const [url, form] = this.#generateTokenRefreshUrl(token);
+
       const response = await fetch(url, { body: form, method: "POST" });
 
       if (response.ok) {
         const json = await response.json();
-        this.#saveToken(json);
-        return this.#currentAccount;
+        return this.#saveToken(json);
       }
-    } else {
-      throw "no refresh token available";
+
+      return errorResult("error during update of token", response);
+    } catch (err) {
+      return errorResult("error during update of token", err);
     }
   }
 
-  public async activate() {
+  /**
+   * runs redirect login
+   * @returns 
+   */
+  public async loginRedirect() { // todo, add option to force prompt etc
+    const urlResult = await this.#generateLoginUrl();
+    if (urlResult.state === "error") {
+      return urlResult;
+    }
+
+    window.open(urlResult.data, "_self");
+
+    // we return always error.. but it will be redirected..
+    return errorResult("redirecting, please wait", null);
+  }
+
+  public async init() {
     const hash = location.hash.replace("#", "");
     let code = new URLSearchParams(hash).get("code");
     let state = new URLSearchParams(hash).get("state");
 
     const oldState = sessionStorage.getItem(this.#stateLocalStorageKey);
     const oldVerifier = sessionStorage.getItem(this.#verifierLocalStorageKey);
-    const useOldState = sessionStorage.getItem(this.#useOldStateLocalStorageKey);
+    const useOldState = sessionStorage.getItem(
+      this.#useOldStateLocalStorageKey
+    );
 
     if (useOldState) {
       // browser reloaded..
@@ -63,19 +103,25 @@ export class MsPkceFlowJs {
 
     if (code && oldState && oldVerifier && oldState === state) {
       const oldurl = sessionStorage.getItem(this.#lasturlLocalStorageKey);
+
       if (oldurl) {
         sessionStorage.removeItem(this.#lasturlLocalStorageKey);
-        // need to save this since we will reload browser one more time..
+
+        // need to save this since we will reload browser one more time setting back old URL before login
         sessionStorage.setItem(this.#useOldStateLocalStorageKey, "TRUE");
         sessionStorage.setItem(this.#codeLocalStorageKey, code);
 
         const urlObj = JSON.parse(oldurl);
         location.href = `${location.origin}${urlObj.pathname}${urlObj.search}${urlObj.hash}`;
-        return;
+
+        // editing location.href will cause reload, but just to make sure, we return/stop here
+        return errorResult("Still logging in, redirect started", null);
       }
       sessionStorage.removeItem(this.#useOldStateLocalStorageKey);
 
-      const url = `https://login.microsoftonline.com/${this.#authConfig.tenant_id}/oauth2/v2.0/token`;
+      const url = `https://login.microsoftonline.com/${
+        this.#authConfig.tenant_id
+      }/oauth2/v2.0/token`;
 
       const form = new FormData();
       form.append("scope", this.#authConfig.scope);
@@ -86,30 +132,34 @@ export class MsPkceFlowJs {
       form.append("code_verifier", oldVerifier);
 
       //get token
-      const response = await fetch(url, { body: form, method: "POST" });
 
-      //cleanup
-      sessionStorage.removeItem(this.#verifierLocalStorageKey);
-      sessionStorage.removeItem(this.#stateLocalStorageKey);
+      try {
+        const response = await fetch(url, { body: form, method: "POST" });
 
-      if (response.ok) {
-        const json = await response.json();
+        //cleanup
+        sessionStorage.removeItem(this.#verifierLocalStorageKey);
+        sessionStorage.removeItem(this.#stateLocalStorageKey);
 
-        this.#saveToken(json);
-        await this.reSheduleRefresh();
+        if (response.ok) {
+          const json = await response.json();
 
-        return json as TokenResult;
-      } else {
-        console.error(response.status, response.statusText);
-        return null;
+          return this.#saveToken(json);
+        } else {
+          console.error(response.status, response.statusText);
+          return errorResult(
+            "Error during code verifying, response not OK",
+            response
+          );
+        }
+      } catch (err) {
+        return errorResult("Error during code verifying, fetch error", err);
       }
     } else {
       if (oldVerifier && oldState) {
         sessionStorage.removeItem(this.#verifierLocalStorageKey);
         sessionStorage.removeItem(this.#stateLocalStorageKey);
         sessionStorage.removeItem(this.#useOldStateLocalStorageKey);
-
-        throw "auth failed";
+        return errorResult("auth failed", null);
       } else {
         sessionStorage.setItem(
           this.#lasturlLocalStorageKey,
@@ -126,164 +176,182 @@ export class MsPkceFlowJs {
 
           this.#currentAccount = JSON.parse(account) as TokenResult;
 
-          await this.reSheduleRefresh();
-
           // might never get here..
-          return this.#currentAccount;
+          return successResult(this.#currentAccount);
         } else {
-          await this.#loginRedirect();
+          return this.loginRedirect();
         }
       }
     }
   }
 
-  private log(msg: string) {
-    if (this.useLog) {
-      console.log(msg);
-    }
-  }
 
-  private async reSheduleRefresh() {
-    if (!this.#currentAccount) {
-      await this.#loginRedirect();
-      return;
-    }
+  // will remove, need to add helpers for expire etc
+  // private async reSheduleRefresh() {
+  //   if (!this.#currentAccount) {
+  //     await this.loginRedirect();
+  //     return;
+  //   }
 
-    const expireDate = new Date(this.#currentAccount.expire_isoString).getTime();
-    const currentDate = new Date().getTime();
+  //   const expireDate = new Date(this.#currentAccount.expire_isoString).getTime();
+  //   const currentDate = new Date().getTime();
 
-    const expireMs = expireDate - currentDate;
-    const buffer5Min = 1000 * 60 * 5;
-    const expireMsMinusBuffer = expireMs- buffer5Min
+  //   const expireMs = expireDate - currentDate;
+  //   const buffer5Min = 1000 * 60 * 5;
+  //   const expireMsMinusBuffer = expireMs- buffer5Min
 
-    // if expired we want to redirect
-    // I prb could improve this, refresh token last 24hours or 90 days, so maybe just refresh if needed
-    if (expireMs < 0) {
-      await this.#loginRedirect();
-      return;
-    }
+  //   // if expired we want to redirect
+  //   // I prb could improve this, refresh token last 24hours or 90 days, so maybe just refresh if needed
+  //   if (expireMs < 0) {
+  //     await this.#loginRedirect();
+  //     return;
+  //   }
 
-    // if its past our buffer we also want to update
-    if (expireMsMinusBuffer < 0) {
-      await this.updateToken().catch(async () => {
-        await this.#loginRedirect();
-        return;
-      });
-    }
+  //   // if its past our buffer we also want to update
+  //   if (expireMsMinusBuffer < 0) {
+  //     await this.updateToken().catch(async () => {
+  //       await this.#loginRedirect();
+  //       return;
+  //     });
+  //   }
 
-    //callback, since its still valid
-    this.#callback(this.#currentAccount);
+  //   //callback, since its still valid
+  //   this.#callback(this.#currentAccount);
 
-    this.log(`scheduled refresh in ${expireMsMinusBuffer / 1000 / 60}`);
+  //   this.log(`scheduled refresh in ${expireMsMinusBuffer / 1000 / 60}`);
 
-    setTimeout(async () => {
-      await this.updateToken().catch(async () => {
-        // maybe now what we want..
-        sessionStorage.setItem(
-          this.#lasturlLocalStorageKey,
-          JSON.stringify({
-            pathname: location.pathname,
-            search: location.search,
-            hash: location.hash,
-          })
-        );
-        await this.#loginRedirect();
-        return;
-      });
+  //   setTimeout(async () => {
+  //     await this.updateToken().catch(async () => {
+  //       // maybe now what we want..
+  //       sessionStorage.setItem(
+  //         this.#lasturlLocalStorageKey,
+  //         JSON.stringify({
+  //           pathname: location.pathname,
+  //           search: location.search,
+  //           hash: location.hash,
+  //         })
+  //       );
+  //       await this.#loginRedirect();
+  //       return;
+  //     });
 
-      if (this.#currentAccount) {
-        this.#callback(this.#currentAccount);
-      }
-    }, expireMsMinusBuffer);
-  }
+  //     if (this.#currentAccount) {
+  //       this.#callback(this.#currentAccount);
+  //     }
+  //   }, expireMsMinusBuffer);
+  // }
 
-  #saveToken(json: any) {
+  #saveToken(json: TokenResult) {
     if (json.access_token) {
-      json.access_token_tokenDecoded = this.#parseJwt(json.access_token);
+      const parseResult = this.#parseJwt<decodedAccessToken>(json.access_token);
+
+      if (parseResult.state === "error") {
+        return parseResult;
+      }
+
+      json.access_token_tokenDecoded = parseResult.data;
     }
 
-    json.expire_isoString = new Date(
-      json.access_token_tokenDecoded?.exp * 1000
-    ).toISOString();
+    try {
+      json.expire_isoString = new Date(
+        json.access_token_tokenDecoded?.exp * 1000
+      ).toISOString();
 
-    json.expire_localString = new Date(json.expire_isoString);
+      json.expire_localString = new Date(json.expire_isoString);
 
-    this.#currentAccount = json;
+      this.#currentAccount = json;
 
-    sessionStorage.setItem(this.#accountLocalStorageKey, JSON.stringify(json));
+      sessionStorage.setItem(
+        this.#accountLocalStorageKey,
+        JSON.stringify(json)
+      );
+
+      return successResult(json);
+    } catch (err) {
+      return errorResult("Unable to save token", err);
+    }
   }
 
-  async #loginRedirect() {
-    const url = await this.#generateLoginUrl();
-    window.open(url, "_self");
-  }
-
-  #parseJwt(token: string) {
+  #parseJwt<T>(token: string) {
     if (!token) {
-      return {};
+      return errorResult("Missing token", null);
     }
 
-    const base64Url = token.split(".")[1];
-    if (!base64Url) {
-      return {};
+    try {
+      const base64Url = token.split(".")[1];
+      if (!base64Url) {
+        return errorResult("Missing base64Url", null);
+      }
+
+      const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+
+      const jsonPayload = decodeURIComponent(
+        window
+          .atob(base64)
+          .split("")
+          .map(function (c) {
+            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+          })
+          .join("")
+      );
+
+      const parsedPayload = JSON.parse(jsonPayload);
+
+      return successResult<T>(parsedPayload);
+    } catch (err) {
+      return errorResult("Unable to parse token", err);
     }
-
-    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-
-    const jsonPayload = decodeURIComponent(
-      window
-        .atob(base64)
-        .split("")
-        .map(function (c) {
-          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join("")
-    );
-
-    return JSON.parse(jsonPayload);
   }
 
   async #generatePKCE(length = 96) {
-    // collected from misc sources/google searches
-    const randomNumbers = await crypto.getRandomValues(new Uint8Array(length));
+    try {
+      // collected from misc sources/google searches
+      const randomNumbers = await crypto.getRandomValues(
+        new Uint8Array(length)
+      );
 
-    let randomString = "";
-    randomNumbers.forEach((num) => {
-      randomString += String.fromCharCode(num);
-    });
+      let randomString = "";
+      randomNumbers.forEach((num) => {
+        randomString += String.fromCharCode(num);
+      });
 
-    // verifier is something we send to MS after we have gotten a code from them
-    const verifier = window
-      .btoa(randomString)
-      .replace(/\+/g, "-")
-      .replace(/\//g, "_")
-      .replace(/=+$/, "");
-
-    const numArray = new Uint8Array(verifier.length);
-
-    for (let i = 0; i < verifier.length; i++) {
-      numArray[i] = verifier.charCodeAt(i);
-    }
-
-    const digest = await crypto.subtle.digest("SHA-256", numArray);
-
-    const digestBase64String = String.fromCharCode.apply(
-      null,
-      new Uint8Array(digest) as unknown as number[]
-    );
-
-    return {
-      verifier,
-      challenge: window
-        .btoa(digestBase64String)
+      // verifier is something we send to MS after we have gotten a code from them
+      const verifier = window
+        .btoa(randomString)
         .replace(/\+/g, "-")
         .replace(/\//g, "_")
-        .replace(/=+$/, ""),
-    };
+        .replace(/=+$/, "");
+
+      const numArray = new Uint8Array(verifier.length);
+
+      for (let i = 0; i < verifier.length; i++) {
+        numArray[i] = verifier.charCodeAt(i);
+      }
+
+      const digest = await crypto.subtle.digest("SHA-256", numArray);
+
+      const digestBase64String = String.fromCharCode.apply(
+        null,
+        new Uint8Array(digest) as unknown as number[]
+      );
+
+      return successResult({
+        verifier,
+        challenge: window
+          .btoa(digestBase64String)
+          .replace(/\+/g, "-")
+          .replace(/\//g, "_")
+          .replace(/=+$/, ""),
+      });
+    } catch (err) {
+      return errorResult("Error while generating PKCE", err);
+    }
   }
 
   #generateTokenRefreshUrl(token: string) {
-    const authUrl = `https://login.microsoftonline.com/${this.#authConfig.tenant_id}/oauth2/v2.0/token`;
+    const authUrl = `https://login.microsoftonline.com/${
+      this.#authConfig.tenant_id
+    }/oauth2/v2.0/token`;
 
     const form = new FormData();
     form.append("client_id", this.#authConfig.client_id);
@@ -295,13 +363,19 @@ export class MsPkceFlowJs {
   }
 
   async #generateLoginUrl() {
-    const PKCE = await this.#generatePKCE();
+    const pkceResult = await this.#generatePKCE();
+    if (pkceResult.state === "error") {
+      return pkceResult;
+    }
+
     const state = crypto.randomUUID();
 
-    sessionStorage.setItem(this.#verifierLocalStorageKey, PKCE.verifier);
+    sessionStorage.setItem(this.#verifierLocalStorageKey, pkceResult.data.verifier);
     sessionStorage.setItem(this.#stateLocalStorageKey, state);
 
-    const authUrl = `https://login.microsoftonline.com/${this.#authConfig.tenant_id}/oauth2/v2.0/authorize`;
+    const authUrl = `https://login.microsoftonline.com/${
+      this.#authConfig.tenant_id
+    }/oauth2/v2.0/authorize`;
 
     const urlParam = new URLSearchParams();
     urlParam.append("client_id", this.#authConfig.client_id);
@@ -309,13 +383,45 @@ export class MsPkceFlowJs {
     urlParam.append("response_mode", "fragment");
     urlParam.append("scope", this.#authConfig.scope);
     urlParam.append("state", state);
-    urlParam.append("code_challenge", PKCE.challenge);
+    urlParam.append("code_challenge", pkceResult.data.challenge);
     urlParam.append("redirect_uri", this.#authConfig.redirect_uri);
     urlParam.append("code_challenge_method", "S256");
 
-    return authUrl + "?" + urlParam.toString();
+    return successResult(authUrl + "?" + urlParam.toString());
   }
 }
+
+function successResult<T = any>(data: T): ResultSuccess<T> {
+  return {
+    state: "success",
+    data,
+  };
+}
+
+function errorResult<T = any>(msg: string, err: T): ResultError<T> {
+  return {
+    state: "error",
+    error: {
+      msg: msg,
+      err,
+    },
+  };
+}
+
+export type ResultSuccess<R = any> = {
+  state: "success";
+  data: R;
+};
+
+export type ResultError<E = any> = {
+  state: "error";
+  error: {
+    msg: string;
+    err: E;
+  };
+};
+
+export type Result<R, E> = ResultSuccess<R> | ResultError<E>;
 
 export type AuthConfig = {
   client_id: string;
@@ -324,28 +430,30 @@ export type AuthConfig = {
   redirect_uri: string;
 };
 
+export type decodedAccessToken = {
+  aud: string;
+  exp: number;
+  iat: number;
+  iss: string;
+  name: string;
+  nbf: number;
+  oid: string;
+  preffered_username: string;
+  rh: string;
+  roles: string[];
+  sub: string;
+  tid: string;
+  uti: string;
+  ver: string;
+};
+
 export type TokenResult = {
   access_token: string;
   expire_isoString: string;
   expire_localString: Date;
   expires_in: number;
   id_token: string;
-  access_token_tokenDecoded: {
-    aud: string;
-    exp: number;
-    iat: number;
-    iss: string;
-    name: string;
-    nbf: number;
-    oid: string;
-    preffered_username: string;
-    rh: string;
-    roles: string[];
-    sub: string;
-    tid: string;
-    uti: string;
-    ver: string;
-  };
+  access_token_tokenDecoded: decodedAccessToken;
   refresh_token: string;
   scope: string;
   token_type: string;
--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,52 @@
 import { MsPkceFlowJs } from "./MsPkceFlowJs.ts";
 
 async function main() {
+  debugger
 
-
-  let redirect_uri = "http://localhost:8080"
-  if(location.host === "vegarringdal.github.io"){
-    redirect_uri = 'https://vegarringdal.github.io/MsPkceFlowJs/'
+  // some tricks to make it work on github pages too
+  let redirect_uri = "http://localhost:8080";
+  if (location.host === "vegarringdal.github.io") {
+    redirect_uri = "https://vegarringdal.github.io/MsPkceFlowJs/";
   }
 
-
+  //create/configure flow
   const msPkceFlowJs = new MsPkceFlowJs(
     {
+      // config
       // dummy app registration
       client_id: "2bca3844-d481-4fd0-a2fe-53a237ec28ec",
       tenant_id: "0bfdc7b6-077d-4379-9617-c56c6453235b",
       scope: "api://2bca3844-d481-4fd0-a2fe-53a237ec28ec/api",
-      redirect_uri
-    },
-
-    // callback
-    // this prob should be events... and user should be able to subscribe...
-    (result) => {
-      console.log(result);
+      redirect_uri,
     }
   );
 
-  msPkceFlowJs.useLog = true;
-
-  // activate
-  const tokenResult = await msPkceFlowJs.activate().catch((e) => {
-    // something wrong, maybe we should return a object with success or error state ?
-    console.error(e);
-  });
-
   const app = document.getElementById("app");
-
-  if (!tokenResult && app) {
-    app.textContent = "something wrong, see console";
+  if (!app) {
+    console.error("Unable to find app element");
     return;
   }
 
-  if (tokenResult) {
-    const accessTokenString = msPkceFlowJs.getAccessToken();
-    const newToken = await msPkceFlowJs.updateToken();
-
-    console.log("tokenResult:", tokenResult);
-    console.log("accessTokenString:", accessTokenString);
-    console.log("newToken:", newToken);
-
-    if (app) {
-      app.textContent =
-        tokenResult.access_token_tokenDecoded.name || "who are you?";
-    }
+  // init msal flow
+  const tokenResult = await msPkceFlowJs.init();
+  if (tokenResult.state === "error") {
+    app.textContent = tokenResult.error.msg;
+    return;
   }
+
+  // just to show what we have
+  const accessTokenString = msPkceFlowJs.getAccessToken();
+  const newToken = await msPkceFlowJs.updateToken();
+
+
+  console.log("tokenResult:", tokenResult);
+  console.log("accessTokenString:", accessTokenString);
+  console.log("newToken:", newToken);
+  
+  
+
+  app.textContent =
+    (tokenResult.data.access_token_tokenDecoded.name || "?? ") + ", see console for more data";
 }
 
 main();
